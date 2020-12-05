804c577ea7a8ed1e8fafb233a926441acf2aec3d
Will Gnann <wgnann@gmail.com>
2020-12-05 20:18:11 -0300

    Problema com SJRP corrigido.

--- drs.py
+++ drs.py
@@ -1,20 +1,16 @@
 import csv
+import re
 import requests
 import unidecode
 from bs4 import BeautifulSoup
 
-url = 'http://www.saude.sp.gov.br/ses/institucional/departamentos-regionais-de-saude/regionais-de-saude'
-page = requests.get(url)
-parser = BeautifulSoup(page.content)
-text = parser.find("p", {"style": "color: #0000ff"})
-drs = text.find_all("a")
-base = 'http://www.saude.sp.gov.br'
-for dr in drs:
-    link = base + dr.get("href")
-    name = unidecode.unidecode(dr.get_text().split('-')[1].strip().upper())
+def write_region(link, name):
     page = requests.get(link)
-    parser = BeautifulSoup(page.content)
-    cities = parser.find_all("p")[7]
+    parser = BeautifulSoup(page.content, 'lxml')
+    # we will search for "Municípios integrantes:" to find the list
+    title = parser.find("p", text=re.compile('Municípios integrantes:'))
+    siblings = [s for s in title.next_siblings]
+    cities = siblings[3]
     data = []
     for city in cities.text.split("\r\n"):
         tmp = {}
@@ -28,3 +24,14 @@ for dr in drs:
     writer.writeheader()
     for row in data:
         writer.writerow(row)
+
+url = 'http://www.saude.sp.gov.br/ses/institucional/departamentos-regionais-de-saude/regionais-de-saude'
+page = requests.get(url)
+parser = BeautifulSoup(page.content, 'lxml')
+text = parser.find("p", {"style": "color: #0000ff"})
+drs = text.find_all("a")
+base = 'http://www.saude.sp.gov.br'
+for dr in drs:
+    link = base + dr.get("href")
+    name = unidecode.unidecode(dr.get_text().split('-')[1].strip().upper())
+    write_region(link, name)
